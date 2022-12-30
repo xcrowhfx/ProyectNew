d5c485519b06a07ed90252653679fe82269b2f32
KIRA <89268982+xcrowhfx@users.noreply.github.com>
2022-12-30 00:40:40 +0000

    changes

---
+++ readme[EN].nix
@@ -0,0 +1,8 @@
+//
+
+Hello This Is A Program Where I Will Explain How To Make An HTML Program For Beginners Who Are Interested
+To Start In This World Of Programming.
+
+I hope it helps you!
+
+//
---
+++ readme[ESP].nix
@@ -0,0 +1,8 @@
+//
+
+Hola Este Es Un Programa Donde Explicare Como Hacer Un Programa HTML Para Principiantes Que Estan Interesados
+En Comenzar En Este Mundo De Programacion.
+
+Espero Que Les Ayude!
+
+//
